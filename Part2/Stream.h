f27c0ba255527ca53895f53cb99bbec817e9e381
Amit <amither@campus.technion.ac.il>
2020-06-24 22:51:53 +0300

    2

---
+++ Part2/Stream.h
@@ -0,0 +1,46 @@
+//
+// Created by Amit on 6/24/2020.
+//
+
+#ifndef OOP_HW5_STREAM_H
+#define OOP_HW5_STREAM_H
+
+#include <stdio.h>
+#include <algorithm>
+#include <functional>
+#include <vector>
+
+template <typename M>
+class Stream{
+private:
+    std::function<std::vector<Stream<M>*>()> block;
+    template <class T>
+    struct collection{
+        constexpr static T* result = new T;
+    };
+
+public:
+
+    Stream<M> static of(M& buffer){
+        collection<buffer>::result;
+    }
+
+
+
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif //OOP_HW5_STREAM_H
